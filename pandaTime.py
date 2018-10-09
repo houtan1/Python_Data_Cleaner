79053ed270d8fbbfdf14e4f54211a486c44ccd5d
Houtan Emad <houtan1@users.noreply.github.com>
2018-10-08 22:28:25 -0700

    Goals 1, 2, and 3

--- pandaTime.py
+++ pandaTime.py
@@ -6,14 +6,35 @@ df = pd.read_table('data/Doering-etal_2018-strip.tab')
 print(df)
 
 # show if any values in the dataframe are null
-isNullDf = pd.isna(df)
-print(isNullDf)
+#isNullDf = pd.isna(df)
+#print(isNullDf)
+
+# Goal 1. Remove any duplicated columns
+# the drop_duplicates() function of pandas removes rows with duplicated values
+# to make this work for columns, we'll transpose the table first so columns become rows that we can detect duplications of
+df = df.T.drop_duplicates().T
+
+# Goal 2. Remove any duplicated rows
+# use the regular instance of the drop_duplicates() functions
+df = df.drop_duplicates()
+
+# Goal 3. Spot/Drop rows with missing field(s)
+df = df.dropna()
+
+# Goal 4. Field format inconsistencies
+# print(df.dtypes)
+# print(df.info())
+# print(df['Depth water [m]'] + df['Temp [°C]'] + df['Longitude'])
+# note, all columns show up as "object" type which is equivalent to string
+# but adding those strings behaves like adding numbers...
+
+# Goal 5. Illogical fields values and outliers
 
 # Filter out all rows that have at least one value outside the standard deviation
 # df = df[(dfnum_bool).all(axis=1)]
 
 # print out the different types for each column
-print(df.dtypes)
+# print(df.dtypes)
 
 # parse our year column as datetime format
 # print(pd.to_datetime(df))
