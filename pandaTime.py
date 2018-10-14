b33f553bca3ace3e96bf04264b3683c1f0e12eab
Houtan Emad <houtan1@users.noreply.github.com>
2018-10-13 17:13:34 -0700

    implementing the rest of Stephen's code

--- pandaTime.py
+++ pandaTime.py
@@ -1,5 +1,6 @@
 # import the pandas module
 import pandas as pd
+import numpy as np
 
 # read in the stripped data into a dataframe and print it
 df = pd.read_table('data/Doering-etal_2018-strip.tab')
@@ -12,6 +13,7 @@ print(df)
 # Goal 1. Remove any duplicated columns
 # the drop_duplicates() function of pandas removes rows with duplicated values
 # to make this work for columns, we'll transpose the table first so columns become rows that we can detect duplications of
+# note: transposing tables is very taxing, and we have to do it twice for this operation
 df = df.T.drop_duplicates().T
 
 # Goal 2. Remove any duplicated rows
@@ -29,10 +31,18 @@ df = df.dropna()
 # but adding those strings behaves like adding numbers...
 
 # Goal 5. Illogical fields values and outliers
+# first, drop all non-numeric columns
+dfnum = df.select_dtypes([np.number])
+# note: this does not work so well...
+
+# then create a table where each entry is marked as true or false based on whether the value falls within 3 standard
+# deviations of every other value in that column
+dfnum_bool = np.abs(dfnum - dfnum.mean()) <= (3 * dfnum.std())
 
 # Filter out all rows that have at least one value outside the standard deviation
-# df = df[(dfnum_bool).all(axis=1)]
+df = df[(dfnum_bool).all(axis=1)]
 
+print(df)
 # print out the different types for each column
 # print(df.dtypes)
 
