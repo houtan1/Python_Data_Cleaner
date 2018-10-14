a805a52911788263820b3a22c1690462dadcfddd
sbates <tagg007@gmail.com>
2018-10-14 09:29:39 -0700

    Added very brittle code to extract the DateTime from multiple fields

--- ml.py
+++ ml.py
@@ -1,12 +1,16 @@
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
+from time import strptime
 from scipy import stats
 
 path = "data/larvae.tab"
 df = pandas.read_table(path, index_col=False)
 
 #dataset = dataset.drop(columns=['Event', 'Longitude', 'Longitude.1', 'Station', 'Year', 'Month'])
+
+# Lowercase column names for ease of use
+df.columns = df.columns.str.lower()
 
 # FILTER STAGE 1: REMOVE DUPLICATE COLUMNS
 
@@ -28,11 +32,29 @@ df = df.dropna()
 
 # FILTER STAGE 4: FIELD FORMAT INCONSISTENCIES
 
-#TODO
+# Convert separate year, month, day fields to a single DateTime column
+if 'year' in df.columns and 'month' in df.columns:
+	# Add a day column if missing
+	if 'day' not in df.columns:
+		df["day"] = 1
+
+	# If month is of type object, convert to string
+	if df["month"].dtype == np.object:
+		df["month"] = df["month"].apply(lambda x: strptime(str(x),'%B').tm_mon)
+
+	# Use Pandas to convert string fields to dates
+	df["date"] = pandas.to_datetime(dict(year=df["year"], month=df["month"], day=df["day"]), errors='ignore')
+
+	# Drop unneeded columns
+	df = df.drop(columns=["year", "month", "day"])
+
+# TODO: Numbers
+
+# TODO: Boolean
 
 # FILTER STAGE 5: ILLOGICAL VALUES OR OUTLIERS
 
-# Drop all non-numeric columns
+# Temporarily drop all non-numeric columns
 dfnum = df.select_dtypes([np.number])
 
 # Create a table where each entry is marked as true or false based on whether the value falls within 3 standard
